3c216756d71ce1e9e6e9fdd51c85cef783dd032d
mizan.zicore <mizan.zicore@gmail.com>
2025-08-11 18:26:16 +0600

    test deply

--- src/shared/footer/Footer.js
+++ src/shared/footer/Footer.js
@@ -4,8 +4,8 @@ const footer = () => {
     <div className="max-w-6xl mx-auto pt-6 border-t border-[#2626261F] flex justify-between items-center text-[#262626] px-10">
       <div>Copyright © 2022 Lorem Ipsum.</div>
       <div className="flex justify-end items-center gap-4 divide-x-2 divide-[#262626]">
-        <a href="#">Privacy Policy</a>
-        <a href="#" className="pl-4">
+        <a href="/">Privacy Policy</a>
+        <a href="/" className="pl-4">
           Terms and Conditions
         </a>
       </div>
